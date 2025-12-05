1807944a8ad5f008922d2d7934df254c34387047
GitHub Action <action@github.com>
2025-12-05 12:19:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/yansioux/jetbrainsthemecalm/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/yansioux/jetbrainsthemecalm/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.yansioux.jetbrainsthemecalm.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.yansioux.jetbrainsthemecalm.MyBundle
+import com.github.yansioux.jetbrainsthemecalm.services.MyProjectService
 import javax.swing.JButton
 
 
